ad56dba16a225dac11941bec381822e560ef521d
Ramon Valeriano <rrvaleriano@gmail.com>
2025-06-20 14:29:56 -0300

    feat(main.py): Criando configuração de temperatura, top p, top k, tokens máximo de saída e tipo de resposta

--- main.py
+++ main.py
@@ -13,12 +13,21 @@ genai.configure(api_key=GEMINI_API_KEY)
 prompt_sistem = "Liste apenas os nomes dos produto, e ofereça uma breve descrição."
 
 
+configuracao_modelo = {
+    "temperature": 2.0,
+    "top_p": 0.9,
+    "top_k": 64,
+    "max_output_tokens": 8192,
+    "response_mime_type": "text/plain"
+}
+
 """
 LLMs Large Linguage Model, ou Grande modelo de linguagem.
 """
 llm = genai.GenerativeModel(
     model_name=MODELO_ESCOLHIDO,
-    system_instruction=prompt_sistem
+    system_instruction=prompt_sistem,
+    generation_config=configuracao_modelo
 )
 
 pergunta = "Liste trê produtos de moda sustentável para ir ao shopping."
