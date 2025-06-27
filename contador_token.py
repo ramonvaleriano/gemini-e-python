8d88ba9a478008095976d6e8a8e95458e382da16
Ramon Valeriano <rrvaleriano@gmail.com>
2025-06-27 12:07:54 -0300

    feat(contador_token.py): Fechando o custo

--- contador_token.py
+++ contador_token.py
@@ -11,6 +11,19 @@ limites_modelo_flahs = {
     "tokens_saida": model_flash.output_token_limit
 }
 
-print(limites_modelo_flahs)
+print(f"Os limites de tokens são: {limites_modelo_flahs}")
 
+llm = genai.GenerativeModel(
+    f"models/{MODELO_ESCOLHIDO}"
+)
 
+quantidade_tokens = llm.count_tokens("O que é um calça de shopping?")
+
+print(f"A quantidade de token gasto é de: {quantidade_tokens}")
+
+resposta = llm.generate_content("O que é uma calça de shopping?")
+tokens_prompt = resposta.usage_metadata.prompt_token_count
+tokens_resposta = resposta.usage_metadata.candidates_token_count
+
+custo_total = (tokens_prompt * custo_entrada_flash) / 1000000 + (tokens_resposta * custo_saida_flash) / 1000000
+print(f"Custo Total U$ Flash: {custo_total}")
