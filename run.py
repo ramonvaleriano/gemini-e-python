3c0a586434833dca2df42f73371eefb0e320b1fa
Ramon Valeriano <rrvaleriano@gmail.com>
2025-08-11 20:44:50 -0300

    feat(run.py): Ajustando as configurações do modelo seguinte a documentação

--- run.py
+++ run.py
@@ -1,12 +1,17 @@
-from settings import (
-    GEMINI_API_KEY,
-    MODELO_ESCOLHIDO
-)
+from settings import GEMINI_API_KEY, MODELO_ESCOLHIDO
 
 from google import genai
 from google.genai import types
 
 client = genai.Client(api_key=GEMINI_API_KEY)
+
+config_model = {
+    "temperature": 2.0,
+    "top_p": 0.9,
+    "top_k": 64,
+    "max_output_tokens": 8192,
+    "response_mime_type": "text/plain",
+}
 
 question = "Liste três produtos de moda sustentável para ir ao shopping."
 prompt_system = "Liste apenas os nomes dos produto, e ofereça uma breve descrição."
@@ -14,9 +19,14 @@ prompt_system = "Liste apenas os nomes dos produto, e ofereça uma breve descri�
 response = client.models.generate_content(
     model=MODELO_ESCOLHIDO,
     config=types.GenerateContentConfig(
-        system_instruction=prompt_system
+        system_instruction=prompt_system,
+        temperature=config_model["temperature"],
+        top_p=config_model["top_p"],
+        top_k=config_model["top_k"],
+        max_output_tokens=config_model["max_output_tokens"],
+        response_mime_type=config_model["response_mime_type"]
     ),
-    contents=question
+    contents=question,
 )
 
 text_response = response.text
