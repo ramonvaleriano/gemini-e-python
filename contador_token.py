8e5babe385d91999ccd2a45be2287347d189442d
Ramon Valeriano <rrvaleriano@gmail.com>
2025-08-15 17:52:23 -0300

    feat(contador_token.py e contador_token_doc.py): Criando validador de custos

--- contador_token.py
+++ contador_token.py
@@ -28,12 +28,29 @@ print(f"Limites para o modelo Gemma: {limites_model_gemma}")
 print(f"Limites para o modelo Flash Learn: {limites_model_flash_learn}")
 print("\n")
 
+question = "O que é uma calça moderna"
+
 llm_gemma = genai.GenerativeModel(model_name=MODELO_GEMMA)
-quantity_tokens_using_gemma = llm_gemma.count_tokens("O que é uma calça moderna")
+quantity_tokens_using_gemma = llm_gemma.count_tokens(question)
 
 print(f"Quantidade de tokens usando o modelo Gemma: {quantity_tokens_using_gemma}")
 
 llm_flash_learn = genai.GenerativeModel(model_name=MODELO_FLASH_LEARN)
-quantity_tokens_flash_learn = llm_flash_learn.count_tokens("O que é uma calça moderna")
+quantity_tokens_flash_learn = llm_flash_learn.count_tokens(question)
 
 print(f"Quantidade de tokens usando o modelo Flash Learn: {quantity_tokens_flash_learn}")
+
+response_gemma = llm_gemma.generate_content(question)
+tokens_prompt_gemma = response_gemma.usage_metadata.prompt_token_count
+tokens_response_gemma = response_gemma.usage_metadata.candidates_token_count
+
+custo_tota_gemma = (tokens_prompt_gemma * CUSTO_ENTRADA_GEMMA) / 1000000 + (tokens_response_gemma * CUSTO_SAIDA_GEMMA) / 1000000
+print(f"Custo total para o Gemma: {custo_tota_gemma}")
+
+response_flash_learn= llm_gemma.generate_content(question)
+tokens_prompt_flash_learn = response_gemma.usage_metadata.prompt_token_count
+tokens_response_flash_learn = response_gemma.usage_metadata.candidates_token_count
+
+custo_tota_flash_learn = (tokens_prompt_flash_learn * CUSTO_ENTRADA_FLASH_LEARN) / 1000000 + (tokens_response_gemma * CUSTO_SAIDA_FLASH_LEARN) / 1000000
+print(f"Custo total para o Flash Learn: {custo_tota_flash_learn}")
+
--- contador_token_doc.py
+++ contador_token_doc.py
@@ -2,6 +2,12 @@ from google import genai
 
 from settings import MODELO_FLASH_LEARN, MODELO_GEMMA, GEMINI_API_KEY
 
+CUSTO_ENTRADA_GEMMA = 0.075
+CUSTO_SAIDA_GEMMA = 0.30
+
+CUSTO_ENTRADA_FLASH_LEARN = 3.5
+CUSTO_SAIDA_FLASH_LEARN = 10.0
+
 client = genai.Client(api_key=GEMINI_API_KEY)
 
 model_gemma = client.models.get(model=MODELO_GEMMA)
@@ -22,10 +28,34 @@ print(f"Limites para o modelo Gemma: {limites_model_gemma}")
 print(f"Limites para o modelo Flash Learn: {limites_model_flash_learn}")
 print("\n\n")
 
-quantity_tokens_using_gemma = client.models.count_tokens(model=MODELO_GEMMA, contents="O que é uma calça moderna")
+question = "O que é uma calça moderna"
+
+quantity_tokens_using_gemma = client.models.count_tokens(model=MODELO_GEMMA, contents=question)
 
 print(f"Quantidade de tokens usando o modelo Gemma: {quantity_tokens_using_gemma.total_tokens}")
 
-quantity_tokens_flash_learn = client.models.count_tokens(model=MODELO_FLASH_LEARN, contents="O que é uma calça moderna")
+quantity_tokens_flash_learn = client.models.count_tokens(model=MODELO_FLASH_LEARN, contents=question)
 
 print(f"Quantidade de tokens usando o modelo Flash Learn: {quantity_tokens_flash_learn.total_tokens}")
+
+response_gemma = client.models.generate_content(
+    model=MODELO_GEMMA,
+    contents=question
+)
+
+tokens_prompt_gemma = response_gemma.usage_metadata.prompt_token_count
+tokens_response_gemma = response_gemma.usage_metadata.candidates_token_count
+
+custo_tota_gemma = (tokens_prompt_gemma * CUSTO_ENTRADA_GEMMA) / 1000000 + (tokens_response_gemma * CUSTO_SAIDA_GEMMA) / 1000000
+print(f"Custo total para o Gemma: {custo_tota_gemma}")
+
+response_flash_learn = client.models.generate_content(
+    model=MODELO_FLASH_LEARN,
+    contents=question
+)
+
+tokens_prompt_flash_learn = response_flash_learn.usage_metadata.prompt_token_count
+tokens_response_flash_learn = response_flash_learn.usage_metadata.candidates_token_count
+
+custo_tota_flash_learn = (tokens_prompt_flash_learn * CUSTO_ENTRADA_FLASH_LEARN) / 1000000 + (tokens_response_gemma * CUSTO_SAIDA_FLASH_LEARN) / 1000000
+print(f"Custo total para o Flash Learn: {custo_tota_flash_learn}")
